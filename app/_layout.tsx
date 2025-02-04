07ae348efc3eaf08550305f1885e74e7808865ce
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-02-05 06:47:45 +0800

    safe commmit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,25 +2,23 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
 import App from "./app";
+import React from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function LoadingComponent() {
   
   const [loaded] = useFonts({
-    'SpaceMono': require('../assets/fonts/SpaceMono-Regular.ttf'),
-    'FS Albert-Bold': require('../assets/fonts/FSAlbert-Bold.otf'),
-    'FS Albert-ExtraBold': require('../assets/fonts/FSAlbert-ExtraBold.otf'),
-    'FS Albert-Regular': require('../assets/fonts/FSAlbert-Regular.otf'),
-    'FS Albert-Light': require('../assets/fonts/FSAlbert-Light.otf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    "FS Albert-Bold": require("../assets/fonts/FSAlbert-Bold.otf"),
+    "FS Albert-ExtraBold": require("../assets/fonts/FSAlbert-ExtraBold.otf"),
+    "FS Albert-Regular": require("../assets/fonts/FSAlbert-Regular.otf"),
+    "FS Albert-Light": require("../assets/fonts/FSAlbert-Light.otf"),
   });
   useEffect(() => {
-
     if (loaded) {
       SplashScreen.hideAsync();
-      // router.push("./entry")
     }
   }, [loaded]);
 
@@ -28,11 +26,5 @@ export default function LoadingComponent() {
     return null;
   }
 
-  return (
-    <>
-      <>
-        <App />
-      </>
-    </>
-  );
+  return <App />;
 }
