52a39c19ea69b15b66b672d0587feaa371dcab0e
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-02-05 10:25:49 +0800

    deleted comments

--- app/(web)/_layout.tsx
+++ app/(web)/_layout.tsx
@@ -1,13 +1,8 @@
 import { Slot } from "expo-router";
-import React from "react";  // Import Suspense
-
+import React from "react"; // Import Suspense
 
 // Fallback UI: a loading spinner or any other UI to show while content is loading
 
 export default function WebLayout() {
-  return (
-  
-      <Slot />
-  
-  );
+  return <Slot />;
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,14 +17,9 @@ export default function RootLayout() {
     "FS Albert-Light": require("../assets/fonts/FSAlbert-Light.otf"),
   });
 
-  // State to ensure navigation happens after the layout is mounted
-  const [isMounted, setIsMounted] = useState(false);
-
-  // This effect will ensure the app fully mounts before handling routing
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
-      setIsMounted(true); // Mark as mounted once fonts are loaded
     }
   }, [loaded]);
 
@@ -35,16 +30,13 @@ export default function RootLayout() {
     </View>
   );
 
-  // If fonts aren't loaded yet, display fallback UI with Suspense
   if (!loaded) {
     return <FallbackUI />;
   }
 
-  // Once fonts are loaded, show the main content
   return (
     <Suspense fallback={<FallbackUI />}>
       <Slot />
-      {/* The fallback UI will be shown here while waiting for fonts */}
     </Suspense>
   );
 }
