88a61fb9312d958576beebafeac24e1482bc5a51
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-02-06 11:51:04 +0800

    fixed code

--- app/(app)/(tabs)/settings.tsx
+++ app/(app)/(tabs)/settings.tsx
@@ -1,10 +1,12 @@
 import Container from "@/components/shared/Container";
+import Icon from "@/components/shared/Icon";
 import Label from "@/components/shared/Label";
 import { fonts } from "@/constants/Fonts";
 import { StyleSheet } from "react-native";
 
 const Settings = () => (
   <Container>
+     <Icon name="settings-outline" size={43} color="grey" type="Ionicons" />
     <Label lightColor="grey" style={styles.heading4} text="Settings" />
   </Container>
 );
